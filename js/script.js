a7e75a6775b0c50546b876ab30b1f6c2de388972
darmia05 <darmia@hawk.iit.edu>
2025-03-06 22:41:36 -0600

    Update script.js
    Added script file
    
    Added event listener for form submission
    
    Added custom confirmation alert on form submission
    
    Made it reset form fields after submission
    
    Confirmation alert to include full name and username inputs

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("signup-form");
     
     form.addEventListener("submit", (event) => {
-      event.preventDefault(); // Prevent the default form submission
-  
-      const fullname = document.getElementById("full name").value;
-      const username = document.getElementById("username").value;
-      alert(`Welcome ${fullname}! Your Siege username, ${username}, has been registered successfully.`);
-      
-      // Reset the form
-      form.reset();
+        event.preventDefault(); // Prevent the default form submission
+
+        // Get input values correctly
+        const fullname = document.getElementById("full-name").value;
+        const username = document.getElementById("username").value;
+
+        alert(`Welcome ${fullname}! Your Siege username, ${username}, has been registered successfully.`);
+
+        // Reset the form
+        form.reset();
     });
-  });  
+});
